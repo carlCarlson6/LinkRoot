5ebb4f4c5c2b0759bfd45946c03f044b388d09aa
carlos.acitores.deval <bullet.carlos@gmail.com>
2022-11-13 12:48:50 +0100

    init fronted project

---
+++ fronted/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App';
+import './assets/index.css'
+import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />
+	}
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  	<React.StrictMode>
+		<RouterProvider router={router} />
+  	</React.StrictMode>
+);
